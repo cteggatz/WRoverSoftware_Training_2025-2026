92daba4d2dbabd54116951db7d52ce7be8ee0d18
Christopher Teggatz <categgatz@wisc.edu>
2025-09-29 18:31:00 -0500

    layed out the basics for the drive controller file

--- src/Teleop/XBOX/wr_xbox_controller/wr_xbox_controller/drive_controller.py
+++ src/Teleop/XBOX/wr_xbox_controller/wr_xbox_controller/drive_controller.py
@@ -1,1 +1,40 @@
+# The file that will handle the collection of data I guess?
+import rclpy
+import rclpy.node import Node
+import std_msgs.msg import String
 
+# This is the publisher node that will send out the controller data
+class ControllerHandler(Node):
+    #
+    def __init__(self):
+        # call the node constructor
+        super().__init__("controler_handler")
+        
+        # IDK wtf thise does  
+        self.publisher = self.create_publisher(String, "topic", 10)
+        
+        # Set a event loop?
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    # Feedback look 
+    def timer_callback(self):
+        # get information        
+
+
+
+
+
+def main(args = None):
+    rclpy.init(args = args)
+    
+    #Start the node
+    rclpy.spin(ControlHandler())
+
+    #Cleans up automatically
+
+
+
+
+if __name__ == "__main__":
+    main()
